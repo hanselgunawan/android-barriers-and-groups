d111b2511f03765f8bd836ce85eaf0b9b95f68b4
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-12-13 21:56:45 -0800

    added groups

--- app/src/main/java/com/hanseltritama/barriesandgroups/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/barriesandgroups/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hanseltritama.barriesandgroups
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val group: Group = findViewById(R.id.group)
+        group.visibility = View.GONE
     }
 }
